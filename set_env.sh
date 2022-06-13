10ea64be49b24d58eb6fccd3a669035dd7457625
FSO-HANGNT60\User <ngohang.developers@gmail.com>
2022-06-13 10:00:53 +0700

    config enviroment

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=HangNT60
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=bucket-988376495408-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=XiBHDtbrmwppnEX5uO0pEJ2pvCiqIH8Do4CKsqNn
+export URL=http://localhost:8100
