6c3bc4708ba417e2b5f9781cbbd466286f147cc7
FSO-HANGNT60\User <ngohang.developers@gmail.com>
2022-06-18 17:39:45 +0700

    Remove account

--- deploy_local.sh
+++ deploy_local.sh
@@ -4,31 +4,27 @@ docker-compose up
 # udagram-frontend
 docker build -t udagram-frontend ./udagram-frontend
 docker tag udagram-frontend hang050697/udagram-frontend:v13
-echo "Hang0762512699" | docker login -u "hang050697" --password-stdin
 docker push hang050697/udagram-frontend:v13
 
 
 #udagram-api-user
 docker build -t udagram-api-user ./udagram-api-user
-docker tag udagram-api-user hang050697/udagram-api-user:v9
-echo "Hang0762512699" | docker login -u "hang050697" --password-stdin
-docker push hang050697/udagram-api-user:v9
+docker tag udagram-api-user hang050697/udagram-api-user:v10
+docker push hang050697/udagram-api-user:v10
 
 
 #udagram-api-feed
 docker build -t udagram-api-feed ./udagram-api-feed
-docker tag udagram-api-feed hang050697/udagram-api-feed:v9
-echo "Hang0762512699" | docker login -u "hang050697" --password-stdin
-docker push hang050697/udagram-api-feed:v9
+docker tag udagram-api-feed hang050697/udagram-api-feed:v11
+docker push hang050697/udagram-api-feed:v11
 
 #reverseproxy
 docker build -t udagram-reverseproxy ./udagram-reverseproxy
 docker tag udagram-reverseproxy hang050697/reverseproxy:v9
-echo "Hang0762512699" | docker login -u "hang050697" --password-stdin
 docker push hang050697/reverseproxy:v9
 #check bug :kubectl describe pod backend-feed-6897c6c74c-2sf7f
 # kubectl set image deployment frontend frontend=hang050697/udagram-frontend:v13
- kubectl set image deployment backend-feed backend-feed=hang050697/udagram-api-user:v9
-  kubectl set image deployment backend-user backend-user-user=hang050697/udagram-api-user:v9
+ kubectl set image deployment backend-feed backend-feed=hang050697/udagram-api-feed:v10
+  kubectl set image deployment backend-user backend-user=hang050697/udagram-api-user:v10
 kubectl get service
 kubectl get pods
--- deployment.sh
+++ deployment.sh
@@ -20,4 +20,6 @@ kubectl expose deployment reverseproxy --type=LoadBalancer --name=publicreversep
 # Nếu update cluster
 # aws eks --region us-east-1 update-kubeconfig --name clusterProject3
 #kubectl autoscale deployment backend-feed --cpu-percent=70 --min=3 --max=5
-# kubectl get configmap/env-config -oyaml
+# kubectl get ConfigMap/env-config -oyaml
+
+docker run --rm --publish 8080:8080 -v $HOME/.aws:/root/.aws --env POSTGRESS_HOST=$POSTGRESS_HOST --env POSTGRESS_USERNAME=$POSTGRESS_USERNAME --env POSTGRESS_PASSWORD=$POSTGRESS_PASSWORD --env POSTGRESS_DATABASE=$POSTGRESS_DATABASE --env AWS_REGION=$AWS_REGION --env AWS_PROFILE=$AWS_PROFILE --env AWS_BUCKET=$AWS_BUCKET --env JWT_SECRET=$JWT_SECRET --name cd0354-monolith-to-microservices-project_backend-feed_1
