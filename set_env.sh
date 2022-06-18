2aae87d5d2c37d50f9e54143abaeaaa66b2b49c3
FSO-HANGNT60\User <ngohang.developers@gmail.com>
2022-06-18 17:29:38 +0700

    Fixbug

--- set_env.sh
+++ set_env.sh
@@ -4,9 +4,11 @@ export POSTGRES_USERNAME=HangNT60
 export POSTGRES_PASSWORD=05061997
 export POSTGRES_HOST=cdrinstance.cuoknlao52gn.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=bucket-988376495408-dev
+export AWS_BUCKET=arn:aws:s3:::bucket-988376495408-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=KkzEnAe5cr5XYi/7zFKupB+kJhqgvb2USc1ytDrE
 export URL=http://localhost:8100
 export AWS_SDK_LOAD_CONFIG=1
+
+
