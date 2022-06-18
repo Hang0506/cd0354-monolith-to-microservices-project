e6cee4a6da1fee2baa5f773717e038cdb97be424
FSO-HANGNT60\User <ngohang.developers@gmail.com>
2022-06-18 16:17:02 +0700

    config

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -11,8 +11,6 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
 
 (async () => {
   await sequelize.addModels(V0_FEED_MODELS);
-
-  console.debug("Initialize database connection...");
   await sequelize.sync();
 
   const app = express();
@@ -20,20 +18,16 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
 
   app.use(bodyParser.json());
 
-  // We set the CORS origin to * so that we don't need to
-  // worry about the complexities of CORS this lesson. It's
-  // something that will be covered in the next course.
-  app.use(cors({
+ app.use(cors({
     allowedHeaders: [
       'Origin', 'X-Requested-With',
       'Content-Type', 'Accept',
       'X-Access-Token', 'Authorization',
     ],
     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    preflightContinue: true,
-    origin: '*',
+    origin: "*",
   }));
-
+  
   app.use('/api/v0/', IndexRouter);
 
   // Root URI call
