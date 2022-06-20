a614ea332a2c0472468cc93d05927326197d7321
FSO-HANGNT60\User <ngohang.developers@gmail.com>
2022-06-20 08:56:59 +0700

    note more

--- deployment.sh
+++ deployment.sh
@@ -24,3 +24,7 @@ kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/late
 kubectl edit hpa   kubectl delete hpa backend-user  kubectl delete hpa backend-feed kubectl autoscale deployment backend-feed --cpu-percent=70 --min=3 --max=5
 
 docker run --rm --publish 8080:8080 -v $HOME/.aws:/root/.aws --env POSTGRESS_HOST=$POSTGRESS_HOST --env POSTGRESS_USERNAME=$POSTGRESS_USERNAME --env POSTGRESS_PASSWORD=$POSTGRESS_PASSWORD --env POSTGRESS_DATABASE=$POSTGRESS_DATABASE --env AWS_REGION=$AWS_REGION --env AWS_PROFILE=$AWS_PROFILE --env AWS_BUCKET=$AWS_BUCKET --env JWT_SECRET=$JWT_SECRET --name cd0354-monolith-to-microservices-project_backend-feed_1
+
+sau khi lam xong nên xóa để k bị ảnh hưởng tới việc tính phí 
+kubectl delete --all service
+kubectl delete --all deployment
